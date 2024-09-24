aed1659cbbbcd7d9ab119b4e07652e3e036e80ec
Corey Hughes <Hughes-C109@ulster.ac.uk>
2024-09-24 14:37:07 +0100

    Calculator Program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*static void main (String[] args) {
+
+    Console.WriteLine(Calculation);
+    Console.ReadKey();
+
+}
+
+static void Calculation()
+{
+    int firstNumber = 3;
+    int secondNumber = 11;
+    int total;
+    total = firstNumber + secondNumber;
+    Console.WriteLine(total);
+    
+}
+*/
+
+int firstNumber = 43;
+int secondNumber = 54;
+int total = firstNumber + secondNumber;
+Console.WriteLine("The sum of the two numbers is: " + total);
